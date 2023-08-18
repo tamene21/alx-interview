f63452c90a1a5ac7125fde9e72aa736dc45c4843
tamene gelaye <tamenes21@gmail.com>
2023-08-18 02:06:52 -0700

    rotate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Rotate 2D Matrix
+"""
+
+def rotate_2d_matrix(matrix):
+    """rotate 2D matirx closewise 90 degree
+    args: 
+      matrix (list[[list]]): a matrix
+    """
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            # current number
+            tmp = matrix[i][j]
+            # change top for left
+            matrix[i][j] = matrix[x][i]
+            # change left for bottom
+            matrix[x][i] = matrix[y][x]
+            # change bottom for right
+            matrix[y][x] = matrix[j][y]
+            # change right for top
+            matrix[j][y] = tmp
